4a7783a6016c7a21c6a9fb33a5f8dbfefc6dc965
Lotfullah <85621380+Lotfullah21@users.noreply.github.com>
2023-10-31 23:53:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# jawt-form-api
+npm install jsonwebtoken
+
+## JWT
+
+jwt.sign() takes the payload and secret key as inputs and returns a JWT.
+The resulting JWT is then typically sent to a client after a user logs in. The client can include this JWT in subsequent requests for authentication and authorization purposes.
+The server, in turn, can use the secret key to verify the JWT's authenticity and extract the information from the payload.
--- controllers/jwt.js
+++ controllers/jwt.js
@@ -1,13 +1,23 @@
 const CustomErrorAPI = require("../errors/custom-error");
 
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     throw new CustomErrorAPI("please provide password and name", 400);
   }
-  res.send("Fake/Login");
+  // dummy id
+  const id = new Date().getDate();
+  // in the first position, we add the payload, in later one the secret, and the last one is optional.
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 const dashboard = (req, res) => {
+  console.log(req.headers);
   const randNumber = Math.floor(Math.random() * 100);
   res
     .status(200)
